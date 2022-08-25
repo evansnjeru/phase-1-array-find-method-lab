182e8d89626b05c14b8b602cc89ac9fb0d547163
Evans <evans.njeru@student.moringaschool.com>
2022-08-25 03:15:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function isW(record) {
+	return record.result === 'W';
+}
+function superbowlWin(record) {
+	const x = record.find(isW);
+	return x ? x.year : undefined;
+}
